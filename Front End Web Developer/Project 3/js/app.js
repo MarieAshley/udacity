994429467bbf48b6c605883bfcee3dbef6d4b199
ash <ashley.nohe419@gmail.com>
2019-04-25 09:05:21 -0700

    Player.update & Player.render

--- Front End Web Developer/Project 3/js/app.js
+++ Front End Web Developer/Project 3/js/app.js
@@ -39,8 +39,12 @@ class Player {
         this.x = 0;
         this.y = 0;
     }
-    update() {}
-    render() {}
+    update(dt) {
+        this.x = this.x + (this.speed*dt);
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
     handleInput() {}
 }
 
