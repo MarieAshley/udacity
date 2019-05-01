607631fcab3e8342224159bc5a8060c665d6fe0d
ash <ashley.nohe419@gmail.com>
2019-05-01 08:58:10 -0700

    Cat Clicker, Second Cat
    Added a second cat to the game.

--- Front End Web Developer/Lessons/Cat Clicker/js/main.js
+++ Front End Web Developer/Lessons/Cat Clicker/js/main.js
@@ -1,9 +1,29 @@
 // Main JavaScript file for the Cat Clicker game.
 
-const elem = document.getElementById('cat-image');
-const counterText = document.getElementById('counter');
-let counter = 0;
-elem.addEventListener('click', function(){
-	counter ++;
-	counterText.textContent = `Number of clicks: ${counter}`;
+// Variables to change
+const header1Text = "Mini Tiger";
+const header2Text = "Big Tiger";
+
+// Box 1
+const elem1 = document.querySelector('.box1');
+const header1 = elem1.querySelector('h3');
+header1.textContent = header1Text;
+const image1 = elem1.querySelector('img');
+const counterText1 = elem1.querySelector('p');
+let counter1 = 0;
+image1.addEventListener('click', function() {
+	counter1 ++;
+	counterText1.textContent = `Number of clicks: ${counter1}`;
+}, false);
+
+// Box 2
+const elem2 = document.querySelector('.box2');
+const header2 = elem2.querySelector('h3');
+header2.textContent = header2Text;
+const image2 = elem2.querySelector('img');
+const counterText2 = elem2.querySelector('p');
+let counter2 = 0;
+image2.addEventListener('click', function() {
+	counter2 ++;
+	counterText2.textContent = `Number of clicks: ${counter2}`;
 }, false);
--- Front End Web Developer/Lessons/Cat Clicker/readme.md
+++ Front End Web Developer/Lessons/Cat Clicker/readme.md
@@ -1,7 +1,9 @@
 # Cat Clicker
 ## Project Requirements for Cat Clicker
 ### Visuals
-1. The application should display a picture of a cat and a number of clicks.
-1. The specifics of the layout do not matter, so style it however you'd like.
+- The application should display two cats. Each cat includes
+ - the cat's name
+ - a picture of the cat
+ - text showing the number of clicks
 ### Interaction
-1. The number of clicks should increment when the cat picture is clicked.
+- The number of clicks should increment when each cat picture is clicked.
