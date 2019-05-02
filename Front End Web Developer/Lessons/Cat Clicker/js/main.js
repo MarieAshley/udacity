8806c02d8debad4355734abd6a2afc8e09d87bc2
ash <ashley.nohe419@gmail.com>
2019-05-02 10:01:27 -0700

    ability to select multiple cats
    added ability to select multiple cats

--- Front End Web Developer/Lessons/Cat Clicker/js/main.js
+++ Front End Web Developer/Lessons/Cat Clicker/js/main.js
@@ -1,29 +1,51 @@
 // Main JavaScript file for the Cat Clicker game.
 
-// Variables to change
-const header1Text = "Mini Tiger";
-const header2Text = "Big Tiger";
+// Dictionary of cat headers, image sources, and initial counts
+const cats = {"Big Cat": ["img/bigcat.jpg", 0],
+			  "Angry Cat": ["img/angrycat.jpg", 0],
+			  "Sad Cat": ["img/sadcat.jpg", 0],
+			  "Small Cat": ["img/smallcat.jpg", 0],
+			  "Curious Cat": ["img/curiouscat.jpg", 0]
+			 };
 
-// Box 1
-const elem1 = document.querySelector('.box1');
-const header1 = elem1.querySelector('h3');
-header1.textContent = header1Text;
-const image1 = elem1.querySelector('img');
-const counterText1 = elem1.querySelector('p');
-let counter1 = 0;
-image1.addEventListener('click', function() {
-	counter1 ++;
-	counterText1.textContent = `Number of clicks: ${counter1}`;
-}, false);
+function countTracker(counter, counterText, key) {
 
-// Box 2
-const elem2 = document.querySelector('.box2');
-const header2 = elem2.querySelector('h3');
-header2.textContent = header2Text;
-const image2 = elem2.querySelector('img');
-const counterText2 = elem2.querySelector('p');
-let counter2 = 0;
-image2.addEventListener('click', function() {
-	counter2 ++;
-	counterText2.textContent = `Number of clicks: ${counter2}`;
-}, false);
+	return function(){
+		counter ++;
+		counterText.textContent = `Number of clicks: ${counter}`;
+		cats[key][1] = counter;
+	}
+}
+
+// Build a list of cats
+let catList = document.querySelector(".cat-links ul");
+for (let key in cats){
+	catList.insertAdjacentHTML('afterbegin', 
+		`<li><button type="button">${key}</button></li>`);
+
+	// Finds the first (last added) button 
+	// & creates an event listener to update the header and image source
+	let button = document.querySelector(".cat-links li button");
+	button.addEventListener('click', (function(headerText, imageSrc){
+		return function() {
+			let elem = document.querySelector('.box2');
+			let header = elem.querySelector('h3');
+			header.textContent = headerText;
+			let image = elem.querySelector('img');
+			image.setAttribute("src", imageSrc);
+			let counterText = elem.querySelector('p');
+			counterText.textContent = `Number of clicks: ${cats[headerText][1]}`
+		};
+	})(key, cats[key][0]));
+}
+
+// Sets the event listener on the image
+let image = document.querySelector('.box2 img');
+image.addEventListener('click', function() {
+	let key = document.querySelector('.box2 h3').textContent;
+	let counter = cats[key][1];
+	let counterText = document.querySelector('.box2 p');
+	counter ++;
+	counterText.textContent = `Number of clicks: ${counter}`;
+	cats[key][1] = counter;
+});
